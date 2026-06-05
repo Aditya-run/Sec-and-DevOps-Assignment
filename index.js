830e857a4c6da1ec56cdeed6fec91623061672d6
Aditya-run <adityast473@gmail.com>
2026-06-05 19:38:39 +0530

    added docker file and docker compose file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import express from 'express'
+const express = require("express");
+const app = express();
+const port = 9000;
 
-const app = express()
-const PORT = process.env.PORT ?? 8080
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from the server' })
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 
-app.listen(PORT, () => {
-    console.log(`Server is up and running on PORT ${PORT}`)
-})
+  setTimeout(() => {
+    console.log("Exiting after 3 seconds...");
+    process.exit(0);
+  }, 3000);
+});
